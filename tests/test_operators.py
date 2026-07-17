a7d2f9d6694e0db2a0ecb09dd3ad26b415736fd0
unknown <keshavalalam6@gmail.com>
2026-07-17 13:35:05 +0530

    fixed math logic in tests

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -3,10 +3,10 @@ from src.mathsoperators import add,sub
 def test_add():
     assert add(2,3)==5
     assert add(5,6)==11
-    assert add(5,6)==11
-    assert add(5,3)==7
+    assert add(5,10)==15
+    assert add(5,3)==8
 
 def test_sub():
-    assert add(2,3)==-1
-    assert add(3,3)==0
-    assert add(12,6)==6
+    assert sub(2,3)==-1
+    assert sub(3,3)==0
+    assert sub(12,6)==6
